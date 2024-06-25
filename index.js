217166c7c3658b3d54a809f0128bedda13979c11
ramana <ramanaraoVK18@gmail.com>
2024-06-25 18:27:41 +0530

    first time my code is pushing

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const dotEnv = require("dotenv");
+const mongoose = require("mongoose");
+const vendorRoutes = require('./routes/vendorRoutes');
+const productRoutes = require('./routes/productRoutes');
+const bodyParser = require("body-parser");
+const firmRoutes = require('./routes/firmRoutes')
+const cors = require("cors")
+const path = require('path');
+
+
+
+
+const app = express()
+const PORT  = 7008;
+
+dotEnv.config();
+
+app.use(cors())
+
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Succesfully Connected"))
+    .catch((error) => console.log(error))
+
+app.use(bodyParser.json());
+app.use('/vendor', vendorRoutes);
+app.use('/firm', firmRoutes);
+app.use('/product' , productRoutes);
+app.use('/uploads' , express.static('uploads'));
+
+
+app.listen(PORT, () => {
+    console.log(`server running at ${PORT}`)
+})
+
+app.use('/home',(req,res) => {
+    res.send("<h3> Welcome to QuickBites")
+})
